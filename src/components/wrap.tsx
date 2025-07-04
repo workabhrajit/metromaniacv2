4dcb35dd0795f459d8d6edbed7654e64eb777d01
abhrajit117 <work.abhrajit@gmail.com>
2025-07-04 10:17:10 +0530

    asdfasdasd

---
+++ src/components/wrap.tsx
@@ -0,0 +1,13 @@
+import {
+   
+} from "@/components/ui/card";
+import { WarpBackground } from "@/components/magicui/warp-background";
+import { AnimatedBeamMultipleOutputDemo } from "./all-in-one";
+
+export function ExampleComponentDemo() {
+  return (
+    <WarpBackground>
+      <AnimatedBeamMultipleOutputDemo/>
+    </WarpBackground>
+  );
+}
