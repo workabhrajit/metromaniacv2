1a30e3bae3c45789ad6e1e292d05a3acb1a9ff38
abhrajit117 <work.abhrajit@gmail.com>
2025-07-04 10:23:40 +0530

    asdfasdasd

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/abhrajitghosh/Documents/GitHub/work/metromaniacv2/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
